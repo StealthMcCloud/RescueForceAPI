e97dd9b87ef321598cb59cbd99700376f4530b7b
Carl Smith <carlfsmithiii@gmail.com>
2018-12-31 20:02:02 -0500

    changed db url back to cloud instance

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const dbUser = process.env.DBUSER;
 const dbPassword = process.env.DBPASSWORD;
-// const url = `mongodb://${dbUser}:${dbPassword}@ds241723.mlab.com:41723/rescueforcedb`
-const url = 'mongodb://localhost:27017/RescueForce2';
+const url = `mongodb://${dbUser}:${dbPassword}@ds241723.mlab.com:41723/rescueforcedb`
+// const url = 'mongodb://localhost:27017/RescueForce2';
 
 module.exports.connect = () => {
   return mongoose.connect(
