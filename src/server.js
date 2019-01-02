e831a7e76a88e4d33ade3a93f17c935f25264356
Carl Smith <carlfsmithiii@gmail.com>
2019-01-02 15:06:53 -0500

    get, add, and delete host, shelter, and animal pictures

--- src/server.js
+++ src/server.js
@@ -4,7 +4,6 @@ const { connect } = require("./utils/db");
 const animalRouter = require("./resources/animal/animal.router");
 const hostRouter = require("./resources/host/host.router");
 const shelterRouter = require("./resources/shelter/shelter.router");
-const imageRouter = require('./resources/images/image.router');
 const { register, signin, protect } = require("./utils/auth");
 
 const app = express();
@@ -15,13 +14,9 @@ app.use(morgan("dev"));
 
 app.post("/register", register);
 app.post("/signin", signin);
-// app.use("/api", protect);
 app.use("/api/hosts", protect, hostRouter);
-// app.get('/api/animals', animalRouter);
-// app.get('/api/animals/:id', animalRouter);
 app.use("/api/animals", animalRouter);
 app.use("/api/shelters", protect, shelterRouter);
-app.use('/api/images/', imageRouter);
 
 module.exports.start = async () => {
   try {
