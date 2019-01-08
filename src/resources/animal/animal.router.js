0aeef2304e796b56a77c10743b0977ab824a90f9
Carl Smith <carlfsmithiii@gmail.com>
2019-01-08 15:29:37 -0500

    allow uploads of multiple images

--- src/resources/animal/animal.router.js
+++ src/resources/animal/animal.router.js
@@ -11,13 +11,13 @@ router
   .post(shelterOnly, controllers.createOne);
 
 router
+  .route("/:id/photos")
+  .patch(upload.array("images", 7), controllers.addPhoto);
+
+router
   .route("/:id")
   .get(filters.viewAnimal, controllers.getOne)
   .patch(hostAndShelterOnly, controllers.updateOne)
   .delete(shelterOnly, controllers.removeOne);
-
-router
-  .route("/:id/photos")
-  .patch(upload.array("image", 7), controllers.addPhoto)
 
 module.exports = router;
