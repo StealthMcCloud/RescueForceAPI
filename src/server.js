7da1410b6680eaf6dfae974211b0c6cbf2af68e4
Carl Smith <carlfsmithiii@gmail.com>
2019-01-17 13:24:51 -0500

    use env port

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const {
 } = require("./utils/auth");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
